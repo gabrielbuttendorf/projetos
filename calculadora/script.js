b4c79fb488370a339ecf639c254bb47fc2a924ac
Gabriel Felipe Buttendorf <bielbuttendorf@gmail.com>
2023-10-26 22:09:55 -0300

    finalizando cálculo de porcentagem

--- calculadora/script.js
+++ calculadora/script.js
@@ -1,33 +1,41 @@
-let campoNumerosDigitados = document.getElementById('txt-numeros-digitados')
 let campoResultado = document.getElementById('txt-resultado')
 
 //identificar algum clique ----
 const botoes = document.querySelectorAll('.botao-numero, .botao-operador')
 let numerosDigitados = []
+// let operadores = ['+', '-', '*', '/', '%']
+let ultimaPosicao = 0
 let qtdOperador = 0
+let ultimoBotao = null
+
 
 botoes.forEach(button => {
     button.addEventListener('click', function(event) {
         const valorBotao = event.target.textContent
         const classeBotao = event.target.className
-        
-        if(classeBotao == 'botao-operador' && valorBotao != '%') {
-            qtdOperador++
+        console.log(numerosDigitados)
+        if (classeBotao == 'botao-operador' && ultimoBotao == 'botao-operador') {
+            numerosDigitados.splice(1)
+        } else {
+            if(classeBotao == 'botao-operador' && valorBotao != '%') {
+                qtdOperador++
+            }
         }
+
         if(qtdOperador >= 2) {
-            //arrumar erro de não poder digitar operador 2 vezes seguidas
-            //arumar erro de porcentagem só funciona com adição
+            //se digitar operador antes de algum número da erro (não está dando mais)
+            //passar o resultado da porcentagem pra cá
             let resultado = calcularResultado()
             numerosDigitados = []
             numerosDigitados.push(resultado)
             numerosDigitados.push(valorBotao)
             qtdOperador = 1
-            campoNumerosDigitados.innerText = numerosDigitados.join('')
-            console.log(numerosDigitados)
+            campoResultado.innerText = numerosDigitados.join('')
         } else {
             numerosDigitados.push(valorBotao)
-            campoNumerosDigitados.innerText = numerosDigitados.join('')
+            campoResultado.innerText = numerosDigitados.join('')
         }
+        ultimoBotao = classeBotao
     })
 })
 
@@ -47,7 +55,6 @@ botaoIgual.addEventListener('click', function() {
 //botao de limpar (Ac) ----
 const botaoAc = document.getElementById('btn-ac')
 botaoAc.addEventListener('click', function() {
-    campoNumerosDigitados.innerText = '-'
     campoResultado.innerText = '-'
     numerosDigitados = []
 })
@@ -56,8 +63,7 @@ botaoAc.addEventListener('click', function() {
 const botaoApagar = document.getElementById('btn-apagar')
 botaoApagar.addEventListener('click', function() {
     numerosDigitados.pop()
-    campoNumerosDigitados.innerText = '-'
-    campoNumerosDigitados.innerText = numerosDigitados.join('')
+    campoResultado.innerText = numerosDigitados.join('')
 })
 
 // ------------- não finalizado  -------------
@@ -65,8 +71,13 @@ const botaoPorcento = document.getElementById('btn-porcento')
 botaoPorcento.addEventListener('click', function() {
     const operacao = numerosDigitados.join('')
     const partes = operacao.match(/(\d+(\.\d+)?)([+\-*/])?(\d+(\.\d+)?)/)
-    console.log(partes)
-    campoResultado.innerText = parseFloat(partes[1]) + parseFloat(partes[1] * (partes[4] / 100))
+    //console.log(partes)
+    let teste = `${parseFloat(partes[1])}${partes[3]}${parseFloat(partes[1] * (partes[4] / 100))}`
+    teste = eval(teste)
+    numerosDigitados = []
+    numerosDigitados.push(teste)
+    campoResultado.innerText = teste
+    
     //partes[1] - valor que quer operar
     //partes[3] - sinal da operação principal
     //partes[4] - valor da porcentagem
