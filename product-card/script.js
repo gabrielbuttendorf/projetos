106b5d362577e08dfc81c99832c22d007608efcd
Gabriel Felipe Buttendorf <bielbuttendorf@gmail.com>
2024-02-27 18:41:08 -0300

    slider images on macbook card

--- product-card/script.js
+++ product-card/script.js
@@ -1,24 +1,41 @@
-// SLIDER
-let currentSlide = 0
-const slides = document.querySelectorAll('.slide')
+// SLIDERS
+let currentSlideIphone = 0
+let currentSlideMac = 0
 
-function showSlide(n) {
+function showSlide(slides, n) {
   slides.forEach(slide => slide.classList.remove('active'))
   slides[n].classList.add('active')
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length
-  showSlide(currentSlide)
+//IPHONE
+const iphoneSlides = document.querySelectorAll('.card-iphone .slide')
+
+function nextSlideIphone() {
+  currentSlideIphone = (currentSlideIphone + 1) % iphoneSlides.length
+  showSlide(iphoneSlides, currentSlideIphone)
 }
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length
-  showSlide(currentSlide)
+function prevSlideIphone() {
+  currentSlideIphone = (currentSlideIphone - 1 + iphoneSlides.length) % iphoneSlides.length
+  showSlide(iphoneSlides, currentSlideIphone)
 }
 
-showSlide(currentSlide)
-// FIM SLIDER
+//MACBOOK
+const macSlides = document.querySelectorAll('.card-mac .slide')
+
+function nextSlideMac() {
+  currentSlideMac = (currentSlideMac + 1) % macSlides.length
+  showSlide(macSlides, currentSlideMac)
+}
+
+function prevSlideMac() {
+  currentSlideMac = (currentSlideMac - 1 + macSlides.length) % macSlides.length
+  showSlide(macSlides, currentSlideMac)
+}
+
+showSlide(iphoneSlides, currentSlideIphone)
+showSlide(macSlides, currentSlideMac)
+// FIM SLIDERS
 
 // CHOOSE COLOR
 const iphoneColorList = ['natural', 'white', 'blue', 'black']
@@ -31,9 +48,19 @@ iphoneColors.forEach((colorInput, index) => {
   colorInput.addEventListener('change', () => {
     iphoneColorSelected = iphoneColorList[index]
 
-    slideImage.forEach((imgElement, index) => {
-      imgElement.src = `assets/iphone-${iphoneColorSelected}/image-0${index+1}.png`
+    //TROCA DE CORES + TRANSIÇÃO SUAVE
+    slideImage.forEach(imgElement => {
+      imgElement.style.opacity = 0
     })
+
+    setTimeout(() => {
+      slideImage.forEach((imgElement, index) => {
+        imgElement.src = `assets/iphone-${iphoneColorSelected}/image-0${index+1}.png`
+        imgElement.style.opacity = ''
+      })
+    }, 80)
+    //FIM TROCA DE CORES + TRANSIÇÃO SUAVE
+
   })
 })
 // FIM CHOOSE COLOR
