5ad233fafa4586899dc77b910f69f335352fcb7e
Gabriel Felipe Buttendorf <bielbuttendorf@gmail.com>
2024-03-25 18:36:10 -0300

    changing mode

--- roulette/script.js
+++ roulette/script.js
@@ -1,27 +1,27 @@
 const wheel = document.querySelector('.wheel')
 let value = Math.ceil(Math.random() * 3600)
 
-function spinWheel() {
-  wheel.style.transform = 'rotate(' + value + 'deg)'
-  value += Math.ceil(Math.random() * 3600)
-}
+// function spinWheel() {
+//   wheel.style.transform = 'rotate(' + value + 'deg)'
+//   value += Math.ceil(Math.random() * 3600)
+// }
 
 // ---------------------------------------------------- //
 
-// let chances = 1
+let chances = 1
 
-// if (localStorage.getItem('chances')) {
-//   chances = parseInt(localStorage.getItem('chances'))
-// }
+if (localStorage.getItem('chances')) {
+  chances = parseInt(localStorage.getItem('chances'))
+}
 
-// function spinWheel() {
-//   if (chances > 0) {
-//     wheel.style.transform = 'rotate(' + 2852 + 'deg)'
+function spinWheel() {
+  if (chances > 0) {
+    wheel.style.transform = 'rotate(' + 2852 + 'deg)'
     
-//     chances -= 1
-//     localStorage.setItem('chances', chances)
-//   } else {
-//     alert('Suas chances acabaram.')
-//   }
-// }
+    chances -= 1
+    localStorage.setItem('chances', chances)
+  } else {
+    alert('Suas chances acabaram.')
+  }
+}
 
