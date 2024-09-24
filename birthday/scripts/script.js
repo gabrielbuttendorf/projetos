b3a508b9971e7dc5d1d15c6b1862fadefa521f01
Gabriel Buttendorf <bielbuttendorf@gmail.com>
2024-09-24 20:59:13 -0300

    adicionando todos os sliders

--- birthday/scripts/script.js
+++ birthday/scripts/script.js
@@ -1,18 +1,29 @@
-const carrosselImg = document.querySelectorAll('.carrossel-imagem');
-const tamanhoImg = carrosselImg[0].clientWidth;
-const totalImg = carrosselImg.length;
+const sliders = document.querySelectorAll('.carrossel');
+sliders.forEach((slider, index) => {
+  const carrosselImg = slider.querySelectorAll('.carrossel-imagem');
+  const tamanhoImg = carrosselImg[0].clientWidth;
+  const totalImg = carrosselImg.length;
 
-const carrossel = document.querySelector('.carrossel');
-carrossel.style.width = `${tamanhoImg * totalImg}px`;
+  slider.style.width = `${tamanhoImg * totalImg}px`;
 
-carrossel.animate(
-  [
-    { transform: 'translateX(0)' },
-    { transform: `translateX(${(-tamanhoImg * totalImg) / 2}px)` },
-  ],
-  {
-    duration: 3000,
-    easing: 'linear',
-    iterations: Infinity
+  const isSliderOdd = index % 2 === 1;
+
+  const sliderDirection = isSliderOdd
+    ? [`translateX(${(-tamanhoImg * totalImg) / 2}px)`, 'translateX(0)']
+    : ['translateX(0)', `translateX(${(-tamanhoImg * totalImg) / 2}px)`];
+
+  slider.animate(
+    [{ transform: sliderDirection[0] }, { transform: sliderDirection[1] }],
+    {
+      duration: 3000,
+      easing: 'linear',
+      iterations: Infinity,
+    }
+  );
+
+  const sliderContainer = document.querySelectorAll('.carrossel-container');
+
+  if (isSliderOdd) {
+    sliderContainer[index].classList.add('bordas');
   }
-);
+});
