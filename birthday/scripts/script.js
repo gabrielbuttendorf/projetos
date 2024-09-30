2f9ed996fef718f9a7717b522e647dc41da96e80
Gabriel Buttendorf <bielbuttendorf@gmail.com>
2024-09-30 19:16:46 -0300

    exibir carrosséis ao scroll

--- birthday/scripts/script.js
+++ birthday/scripts/script.js
@@ -1,29 +1,49 @@
-const sliders = document.querySelectorAll('.carrossel');
-sliders.forEach((slider, index) => {
-  const carrosselImg = slider.querySelectorAll('.carrossel-imagem');
-  const tamanhoImg = carrosselImg[0].clientWidth;
-  const totalImg = carrosselImg.length;
+function animarCarrossel() {
+  const sliders = document.querySelectorAll('.carrossel');
+  sliders.forEach((slider, index) => {
+    const carrosselImg = slider.querySelectorAll('.carrossel-imagem');
+    const tamanhoImg = carrosselImg[0].clientWidth;
+    const totalImg = carrosselImg.length;
 
-  slider.style.width = `${tamanhoImg * totalImg}px`;
+    slider.style.width = `${tamanhoImg * totalImg}px`;
 
-  const isSliderOdd = index % 2 === 1;
+    const isSliderOdd = index % 2 === 1;
 
-  const sliderDirection = isSliderOdd
-    ? [`translateX(${(-tamanhoImg * totalImg) / 2}px)`, 'translateX(0)']
-    : ['translateX(0)', `translateX(${(-tamanhoImg * totalImg) / 2}px)`];
+    const sliderDirection = isSliderOdd
+      ? [`translateX(${(-tamanhoImg * totalImg) / 2}px)`, 'translateX(0)']
+      : ['translateX(0)', `translateX(${(-tamanhoImg * totalImg) / 2}px)`];
 
-  slider.animate(
-    [{ transform: sliderDirection[0] }, { transform: sliderDirection[1] }],
-    {
-      duration: 3000,
-      easing: 'linear',
-      iterations: Infinity,
+    slider.animate(
+      [{ transform: sliderDirection[0] }, { transform: sliderDirection[1] }],
+      {
+        duration: 10000,
+        easing: 'linear',
+        iterations: Infinity,
+      }
+    );
+
+    const sliderContainer = document.querySelectorAll('.carrossel-container');
+
+    if (isSliderOdd) {
+      sliderContainer[index].classList.add('bordas');
     }
-  );
+  });
+}
 
-  const sliderContainer = document.querySelectorAll('.carrossel-container');
+function exibirAnos() {
+  const anoCarrosselSection = document.querySelectorAll('.ano-carrossel-section');
+  const alturaTela = window.innerHeight;
 
-  if (isSliderOdd) {
-    sliderContainer[index].classList.add('bordas');
-  }
-});
+  window.addEventListener('scroll', () => {
+    anoCarrosselSection.forEach((section) => {
+      const topDistance = section.getBoundingClientRect().top - alturaTela * 0.6;
+
+      if (topDistance < 0) {
+        section.classList.add('exibir');
+      }
+    });
+  });
+}
+
+animarCarrossel();
+exibirAnos();
