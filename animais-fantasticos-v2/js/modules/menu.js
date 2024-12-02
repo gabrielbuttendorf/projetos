ddd3cf8a7ce29d67598259dbbb64007c3df9683c
Gabriel Buttendorf <bielbuttendorf@gmail.com>
2024-12-02 19:02:18 -0300

    fechar menu ao clicar fora

--- animais-fantasticos-v2/js/modules/menu.js
+++ animais-fantasticos-v2/js/modules/menu.js
@@ -1,3 +1,5 @@
+import outsideClick from './outside-click.js';
+
 export default class Menu {
   constructor(button, list, events) {
     this.menuButton = document.querySelector(button);
@@ -5,6 +7,7 @@ export default class Menu {
     this.events = events || ['click', 'touchstart'];
 
     this.toggleMenu = this.toggleMenu.bind(this);
+    this.clickFora = this.clickFora.bind(this);
   }
 
   toggleMenu() {
@@ -13,9 +16,18 @@ export default class Menu {
     this.menuButton.classList.toggle('ativo');
   }
 
+  clickFora(event) {
+    if (!this.menuButton.contains(event.target) && !this.menuList.contains(event.target)) {
+      this.menuList.classList.remove('ativo');
+      this.menuButton.setAttribute('aria-expanded', false);
+      this.menuButton.classList.remove('ativo');
+    }
+  }
+
   addEvent() {
     this.events.forEach((event) => {
       this.menuButton.addEventListener(event, this.toggleMenu);
+      document.addEventListener(event, this.clickFora)
     });
   }
 
