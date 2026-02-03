fff08d492672b779d2d7233eb2ca2c50a6505922
Gabriel <bielbuttendorf@gmail.com>
2026-02-03 19:57:23 -0300

    finalizando exercício da aposentadoria

--- 100-exercicios-js/92.js
+++ 100-exercicios-js/92.js
@@ -15,29 +15,35 @@ const rl = readline.createInterface({
 });
 
 const anoAtual = new Date().getFullYear();
+const trabalhador = {};
 
 rl.question("Informe o nome do trabalhador: ", (nome) => {
   rl.question("Informe o ano de nascimento: ", (anoNasc) => {
     rl.question("Informe o n° da carteira de trabalho: ", (carteira) => {
-      const trabalhador = {
-        nome,
-        anoNascimento: Number(anoNasc),
-        carteira: Number(carteira),
-        idade: anoAtual - Number(anoNasc),
-      };
+      trabalhador.nome = nome;
+      trabalhador.anoNascimento = Number(anoNasc);
+      trabalhador.carteira = Number(carteira);
+      trabalhador.idade = anoAtual - Number(anoNasc);
 
       if (Number(carteira) !== 0) {
         rl.question("Informe o ano de contratação: ", (anoContrat) => {
           rl.question("Informe o salário: ", (salario) => {
             trabalhador.anoContratacao = Number(anoContrat);
             trabalhador.salario = Number(salario);
+
+            const tempoContribuicao = anoAtual - Number(anoContrat);
+            const idadeAposentadoria = (35 - tempoContribuicao) + trabalhador.idade;
+
+            trabalhador.idadeAposentadoria = idadeAposentadoria;
+
+            console.log("\n", trabalhador);
+            rl.close();
           });
         });
+      } else {
+        console.log(trabalhador);
+        rl.close();
       }
-
-      console.log(trabalhador);
-
-      rl.close();
     });
   });
 });
